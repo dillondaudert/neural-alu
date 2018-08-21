f756e45e5a51b96e00b619a636796f3c12e09ef2
Dillon Daudert <dillongdaudert@gmail.com>
2018-08-21 16:48:53 -0400

    Generate random vectors with sums within specified ranges

--- src/data.jl
+++ src/data.jl
@@ -3,13 +3,36 @@
 using Random
 
 """
+Generate random vectors with constrained magnitudes.
+
+If a_inds (and optionally b_inds) is provided, then
+the magnitude constraints will apply to the subset
+of indices specified by these vectors.
+
+Returns a single vector of values.
+"""
+function randmgn(input_size, min, max, a_inds, b_inds=nothing)
+    while true
+        sample = rand(input_size) .* 2*max .- max
+        asum = sum(sample[a_inds])
+        if b_inds !== nothing
+            bsum = sum(sample[b_inds])
+        end
+        if asum < max && asum > min && (b_inds === nothing || (bsum < max && bsum > min))
+            return sample
+        end
+    end
+end
+
+"""
 Generates a dataset for binary operations.
 """
 function binary_data(bin_op,
                      num_samples::Integer,
                      input_size::Integer,
-                     mgn::Integer,
-                     perm=nothing
+                     mgn::Integer;
+                     perm=nothing,
+                     min_mgn=0,
                     )
     if perm === nothing
         perm = randperm(input_size)
@@ -17,7 +40,7 @@ function binary_data(bin_op,
     a_inds = perm[1:div(input_size, 2)]
     b_inds = perm[div(input_size, 2)+1:end]
     
-    data_xs = [rand(input_size) .* (2*mgn) .- mgn for _ in 1:num_samples]
+    data_xs = [randmgn(input_size, min_mgn, mgn, a_inds, b_inds) for _ in 1:num_samples]
     data_abs = [(sum(x[a_inds]), sum(x[b_inds])) for x in data_xs]
     data_ys = [bin_op(a, b) for (a, b) in data_abs]
     
@@ -34,7 +57,8 @@ function unary_data(op,
                     num_samples::Integer,
                     input_size::Integer,
                     mgn::Integer,
-                    perm=nothing
+                    perm=nothing,
+                    min_mgn=0
                     )
     if perm === nothing
         perm = randperm(input_size)
--- src/test_data.jl
+++ src/test_data.jl
@@ -2,12 +2,31 @@ using Test
 
 include("data.jl")
 
+@testset "randmgn tests" begin
+    
+    for i=1:100
+        input_size = rand(1:100)
+        perm = randperm(input_size)
+        a_inds = perm[1:div(input_size, 2)]
+        b_inds = perm[div(input_size, 2)+1:end]
+        
+        min = 0
+        max = rand(1:100)*rand()
+        val = randmgn(input_size, min, max, a_inds, b_inds)
+        @test sum(val[a_inds]) > min
+        @test sum(val[a_inds]) < max
+        @test sum(val[b_inds]) > min
+        @test sum(val[b_inds]) < max
+    end
+    
+end
+
 @testset "fixed binary_data tests" begin
     input_size = 10
     perm = collect(1:10)
     
     for op in [+, -, *, /]
-        (xs, ys), (_, a_inds, b_inds, abs) = binary_data(op, 100, input_size, 5, perm)
+        (xs, ys), (_, a_inds, b_inds, abs) = binary_data(op, 100, input_size, 5, perm=perm)
         for i in 1:length(xs)
             @test sum(xs[i][1:5]) == abs[i][1]
             @test sum(xs[i][6:10]) == abs[i][2]
@@ -21,7 +40,7 @@ end
     
     for op in [+, -, *, /]
         (xs, ys), (perm, a_inds, b_inds, abs) = binary_data(op, 100, input_size, 10)
-        (xs2, ys2), (perm2, a_inds2, b_inds2, abs2) = binary_data(op, 100, input_size, 10, perm)
+        (xs2, ys2), (perm2, a_inds2, b_inds2, abs2) = binary_data(op, 100, input_size, 10, perm=perm)
         
         @test all(perm .== perm2)
         @test all(a_inds .== a_inds2)
