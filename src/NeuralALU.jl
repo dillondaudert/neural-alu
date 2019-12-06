83b62d4a69d3b730392610469016e821990323fa
Dillon Daudert <dillongdaudert@gmail.com>
2019-12-06 14:47:53 -0500

    Update accumulator

---
+++ src/NeuralALU.jl
@@ -0,0 +1,8 @@
+module NeuralALU
+
+using Flux
+include("nalu.jl")
+
+export NeuralAccumulator
+
+end # module
--- src/nalu.jl
+++ src/nalu.jl
@@ -1,36 +1,42 @@
 # structs and methods for basic neural accumulator / neural arithmetic
 # logic unit.
 
-using Flux
-using NNlib: σ_stable
-
 
 # - Neural Accumulator - #
-struct NAC{S}
-    Ŵ::S
-    M̂::S
+"""
+A linear layer whose weights are soft constrained to be near one of
+{-1, 0, 1}. The weights are calculated by tanh.(W) .* σ.(M).
+"""
+struct NeuralAccumulator{R <: AbstractMatrix}
+    W::R
+    M::R
+    function NeuralAccumulator{R}(w, m) where {R <: AbstractMatrix}
+        size(w) == size(m) || error("sizes of weight matrices must match")
+        new{R}(w, m)
+    end
+end
+NeuralAccumulator(W::R, M::R) where {R <: AbstractMatrix} = NeuralAccumulator{R}(W, M)
+
+function NeuralAccumulator(::Type{T}, in::Integer, out::Integer, init_fn) where {T <: Number}
+    return NeuralAccumulator(init_fn(T, out, in), init_fn(T, out, in))
 end
 
-NAC(in::Integer, out::Integer; initW = Flux.initn) = 
-    NAC(param(initW(out, in)), param(initW(out, in)))
+NeuralAccumulator(in::Integer, out::Integer, init_fn=randn) = NeuralAccumulator(Float64, in, out, init_fn)
 
-Flux.@treelike(NAC)
+operator(nac::NeuralAccumulator) = (2 .* σ.(nac.W) .- 1) .* σ.(nac.M)
 
-function (nac::NAC)(x)
-    W = tanh.(nac.Ŵ) .* σ_stable.(nac.M̂)
-    return W * x
-end
+(nac::NeuralAccumulator)(x) = operator(nac) * x
 
 
 
 # - Neural Arithmetic Logic Unit - #
-struct NALU{S, T}
-    nac::NAC
-    G::S
-    b::T
+struct NeuralALU{R <: AbstractMatrix}
+    nac::NeuralAccumulator{R}
+    G::R
+    b
 end
 
-NALU(in::Integer, out::Integer; initW = Flux.initn, initb = zeros) = 
+NALU(in::Integer, out::Integer; initW = Flux.initn, initb = zeros) =
     NALU(NAC(in, out, initW=initW), param(initW(out, in)), param(initb(out)))
 
 Flux.@treelike(NALU)
@@ -38,13 +44,13 @@ Flux.@treelike(NALU)
 function (nalu::NALU)(x)
     # gate
     g = σ_stable.(nalu.G*x .+ nalu.b)
-    
+
     # addition
     a = nalu.nac(x)
-    
+
     # multiplication
     m = exp.(nalu.nac(log.(abs.(x) .+ eps())))
-    
+
     # nalu
     return g .* a + (1 .- g) .* m
 end
